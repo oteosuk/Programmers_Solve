7ce932b1869929d623d71baf8205750371307d5f
Kwon O Tae <62225124+oteosuk@users.noreply.github.com>
2026-09-18 16:55:10 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 4.80 ms, Memory: 86.6 MB -BaekjoonHub

--- Java/프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ Java/프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String today, String[] terms, String[] privacies) {
-        int total = toDays(today);
+        int todayTotal = getTotal(today);
 
         // 약관 파싱
         Map<String, Integer> termMap = new HashMap<>();
@@ -15,19 +15,19 @@ class Solution {
         List<Integer> answer = new ArrayList<>();
         for (int i = 0; i < privacies.length; i++) {
             String[] parts = privacies[i].split(" ");
-            int expire = toDays(parts[0]) + termMap.get(parts[1]) * 28 - 1;
+            int expire = getTotal(parts[0]) + termMap.get(parts[1]) * 28;
 
-            if (expire < total) answer.add(i + 1);
+            if (expire <= todayTotal) answer.add(i + 1);
         }
-        int[] result = new int[answer.size()];
-        for (int i = 0; i < answer.size(); i++) {
-            result[i] = answer.get(i);   // 여기서 언박싱 자동 발생
-        }
-        return result;
-        //return answer.stream().mapToInt(i -> i.intValue()).toArray();
+        //int[] result = new int[answer.size()];
+        //for (int i = 0; i < answer.size(); i++) {
+            //result[i] = answer.get(i);   // 여기서 언박싱 자동 발생
+        //}
+        //return result;
+        return answer.stream().mapToInt(i -> i.intValue()).toArray();
     }
 
-    private int toDays(String date) {
+    private int getTotal(String date) {
         String[] p = date.split("\\.");
         int y = Integer.parseInt(p[0]);
         int m = Integer.parseInt(p[1]);
